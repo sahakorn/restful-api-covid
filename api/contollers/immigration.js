ee72c1b2cf0092890ea46a3ac021a82827a4b8ad
sahakorn buangam <sahakorn.new@gmail.com>
2020-04-05 16:28:50 +0700

    edit public screening

--- api/contollers/immigration.js
+++ api/contollers/immigration.js
@@ -1,17 +1,31 @@
 const Immigration = require('../models/immigration')
 exports.create_immigration = (req,res,next)=>{
     const req_data = req.body
-    Immigration.create(req_data).then(data =>{
-        return res.status(200).json({
-            message:'User created!',
-            data:data
-        })
-    }).catch(err => {
-        console.log(err.message)
+    Immigration.findAll({where:{pid:req.body.pid}}).then(person=>{
+        if(person.length > 0){
+            return res.status(401).json({
+                message:"conflig"
+            })
+        }else{
+            Immigration.create(req_data).then(data =>{
+                return res.status(200).json({
+                    message:'User created!',
+                    data:data
+                })
+            }).catch(err => {
+                console.log(err.message)
+                return res.status(500).json({
+                    message:err.message
+                })
+            });
+        }
+       
+    }).catch(err=>{
         return res.status(500).json({
             message:err.message
         })
-    });
+    })
+   
 }
 exports.find_immigration = (req,res,next)=>{
     Immigration.findAll().then(data=>{
